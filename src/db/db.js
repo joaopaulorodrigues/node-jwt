3b92bd322e810fc42384399e2f0a3b8a4bb89d73
João Paulo Rodrigues <joaofr13@gmail.com>
2019-11-09 22:51:01 -0300

    Configurando o eslint

--- src/db/db.js
+++ src/db/db.js
@@ -3,5 +3,5 @@ const mongoose = require('mongoose')
 mongoose.connect(process.env.MONGODB_URL, {
     useNewUrlParser: true,
     useCreateIndex: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
 })
--- src/models/User.js
+++ src/models/User.js
@@ -7,33 +7,33 @@ const userSchema = mongoose.Schema({
     name: {
         type: String,
         required: true,
-        trim: true
+        trim: true,
     },
     email: {
         type: String,
         required: true,
         unique: true,
         lowercase: true,
-        validate: value => {
+        validate: (value) => {
             if (!validator.isEmail(value)) {
                 throw new Error({error: 'Invalid Email address'})
             }
-        }
+        },
     },
     password: {
         type: String,
         required: true,
-        minLength: 7
+        minLength: 7,
     },
     tokens: [{
         token: {
             type: String,
-            required: true
-        }
-    }]
+            required: true,
+        },
+    }],
 })
 
-userSchema.pre('save', async function (next) {
+userSchema.pre('save', async function(next) {
     // Hash the password before saving the user model
     const user = this
     if (user.isModified('password')) {
@@ -53,13 +53,13 @@ userSchema.methods.generateAuthToken = async function() {
 
 userSchema.statics.findByCredentials = async (email, password) => {
     // Search for a user by email and password.
-    const user = await User.findOne({ email} )
+    const user = await User.findOne({email} )
     if (!user) {
-        throw new Error({ error: 'Invalid login credentials' })
+        throw new Error({error: 'Invalid login credentials'})
     }
     const isPasswordMatch = await bcrypt.compare(password, user.password)
     if (!isPasswordMatch) {
-        throw new Error({ error: 'Invalid login credentials' })
+        throw new Error({error: 'Invalid login credentials'})
     }
     return user
 }
